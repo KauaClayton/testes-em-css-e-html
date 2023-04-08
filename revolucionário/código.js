cde3c30c9415d8bb608160b6df75c5d93ebb70ef
MefistoCly <121896566+MefistoCly@users.noreply.github.com>
2023-04-08 15:13:47 -0300

    teste novo

--- revolucionário/código.js
+++ revolucionário/código.js
@@ -27,7 +27,7 @@ function clicou(){
         resp.innerHTML = 'Thomas Isidore Noël Sankara foi um militar, revolucionário marxista, pan-africanista e líder político de Burkina Faso. Foi um popular capitão e o primeiro-ministro quando o país ainda se chamava República do Alto Volta.'
     }
     else if(num == 2){
-        artig.style = 'height:490px'
+        artig.style = 'height:510px'
         ima.style = 'Background-image: url("https://upload.wikimedia.org/wikipedia/commons/2/2b/Marighella_foto3_deputado_46-47_apeerj_%28cropped%29.jpg")'
         resp.innerHTML = 'Carlos Marighella foi um político, escritor e guerrilheiro comunista marxista-leninista brasileiro. Um dos principais organizadores da luta armada contra a ditadura militar brasileira, Marighella chegou a ser considerado o inimigo "número um" do regime.'
     }
@@ -47,22 +47,22 @@ function clicou(){
         resp.innerHTML = 'Kim Il-sung foi o líder da Coreia do Norte desde a fundação do país em 1948 até à data da sua morte. Sucedeu-lhe como líder o seu filho, Kim Jong-il. Exerceu o cargo de primeiro-ministro de 1948 a 1972 e de presidente de 1972 até à sua morte. '
     }
     else if(num == 6){
-        artig.style = 'height:460px'
+        artig.style = 'height:470px'
         ima.style = 'Background-image: url("https://ichef.bbci.co.uk/news/640/cpsprodpb/38EB/production/_124717541_gettyimages-104416848.jpg")'
         resp.innerHTML = 'Ho Chi Minh foi um revolucionário, político, escritor, poeta e jornalista vietnamita. Também conhecido por seu nome de batismo Nguyễn Sinh Cung e pelo pseudônimo Nguyen Ai Quoc, foi um revolucionário e estadista vietnamita.'
     }
     else if(num == 7){
-        artig.style = 'height:470px'
+        artig.style = 'height:490px'
         ima.style = 'Background-image: url("https://upload.wikimedia.org/wikipedia/commons/a/a9/Mao_Zedong_1959.jpg")'
         resp.innerHTML = 'Mao Tsé-Tung, em chinês tradicional: 毛澤東; chinês simplificado: 毛泽东; Mao Tsé-Tung pela transliteração Wade-Giles, ou Máo Zédōng, pela pinyin; foi um político, teórico, líder comunista e revolucionário chinês.'
     }
     else if(num == 8){
-        artig.style = 'height:430px'
+        artig.style = 'height:445px'
         ima.style = 'Background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuodHNm-PoMjiB4zwO_9TpCg__X0e6DnSlaU5GXZQevpdoJtRyWYRjlx9iPJvGXP1a79Y&usqp=CAU")'
         resp.innerHTML = 'Virgílio Gomes da Silva foi um operário, sindicalista e guerrilheiro brasileiro que se destacou na luta armada de esquerda no Brasil contra a ditadura militar na década de 1960'
     }
     else if(num == 9){
-        artig.style = 'height:490px'
+        artig.style = 'height:500px'
         ima.style = 'background-image: url("https://s1.static.brasilescola.uol.com.br/be/2021/03/rosa-luxemburgo.jpg")'
         resp.innerHTML = 'Rosa Luxemburgo foi uma filósofa e economista marxista polaco-alemã. Tornou-se mundialmente conhecida pela militância revolucionária ligada à Social-Democracia da Polônia, ao Partido Social-Democrata da Alemanha e ao Partido Social-Democrata Independente da Alemanha.'
     }
