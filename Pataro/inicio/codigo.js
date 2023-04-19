b9e4bb0e33ae60101c8b7c6b621a43ce43262b32
MefistoCly <121896566+MefistoCly@users.noreply.github.com>
2023-04-19 14:31:09 -0300

    fazendo a página inicial
    fazendo a página inicial do site de aniversário da pataro, agora testarei se deu certo no celular

---
+++ Pataro/inicio/codigo.js
@@ -0,0 +1,8 @@
+function pataro(){
+    var resp = document.querySelector('p#res')
+    var garo = document.querySelector('h2#garota')
+    garo.innerHTML = ''
+    var img = document.querySelector('img#img')
+    resp.innerHTML = 'Então saia daqui seu fudido de merda smt senão ficarei descomtrolado aaaaaaaaaaaaaaa'
+    document.querySelector('img#img').src = 'https://uploads.spiritfanfiction.com/historias/capas/201709/tokyo-ghoul-after-kaneki--interativa-10398220-210920172009.jpg'
+}
