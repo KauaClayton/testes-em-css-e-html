5ba513cbcb42a3a343f59c75224d4540c1de39bd
MefistoCly <121896566+MefistoCly@users.noreply.github.com>
2023-04-22 12:59:32 -0300

    me matarei

--- SexKing/Pataro/principal/codigo.js
+++ SexKing/Pataro/principal/codigo.js
@@ -13,7 +13,7 @@ function mudar(){
         resp.innerHTML = 'Pataro, vc é pessoa de cabelo pintado mais boba que conheço Boba é elogio, te amo e te acho top 3 fãs de jojo, brincadeiras a parte vc é incrível e merece o mundo'
         num++
     }
-    else if(num ==2){
+    else if(num == 2){
         foto.src = 'imagem/PataroBianca.jpg'
         resp.innerHTML = "Para a pessoa mais engraçada que eu já conheci Com certeza eu não seria a mesma e não teria tomado decisões tão boas sem você na minha vida, eu não estaria completa sem a minha gêmea siamesa A vida ta cheia de fases ruins, escolhas horríveis, 2019's e Raissas mas se a gente tiver juntas a gente vai passar por tudo isso Obrigada por ser a minha parceira🤞 de mudanças, sejam elas no cabelo, no jeito, nas piadas e na escola, a gente ta junta Eu não poderia ser mais grata por ter você como a minha soulmate Feliz aniversário Pataro Imagem Pública do Casquel🫶"
         num++
